adb554030160eb528cabf9253e0f5679cd12da6a
azathot69 <jacuna98@gmail.com>
2023-11-14 15:32:13 -0800

    Push

--- Bullet Purgatory/Assets/Scripts/SceneControl.cs
+++ Bullet Purgatory/Assets/Scripts/SceneControl.cs
@@ -1,18 +1,45 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class SceneControl : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+
+    //Variables
+    private int MainMenu = 0;
+    private int GameOver = 1;
+    private int WinScreen = 2;
+
+    //Functions
+
+    //Go to main Game
+    public void GoToGame()
     {
-        
+        Debug.Log("The gorilla of ape is yearning");
+        SceneManager.LoadScene(MainMenu);
     }
 
-    // Update is called once per frame
-    void Update()
+    //Player loses and goes to game over screen
+    public void PlayerLose()
     {
-        
+        Debug.Log("There is no crying in video games");
+        SceneManager.LoadScene(GameOver);
     }
+
+    //Player wins the game
+    public void PlayerWins()
+    {
+        Debug.Log("Winner is You");
+        SceneManager.LoadScene(WinScreen);
+    }
+
+    //Player Exits the game
+    public void QuitGame()
+    {
+        Debug.Log("Player Quits Game");
+        Application.Quit();
+    }
+
+   
 }
