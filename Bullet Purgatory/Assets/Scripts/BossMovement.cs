817c8df1b57787a39f88336bf3d576abb2c3017d
azathot69 <jacuna98@gmail.com>
2023-12-03 16:33:12 -0800

    Boss Movement Push
    Boss now moves across the screen and moves to a new spot when reaching off screen. Doesn't do anything else besides that.

--- Bullet Purgatory/Assets/Scripts/BossMovement.cs
+++ Bullet Purgatory/Assets/Scripts/BossMovement.cs
@@ -15,6 +15,9 @@ public class BossMovement : MonoBehaviour
     public float minZ;
     public float maxZ;
 
+    public bool moving = false;
+    public int spawnPos = 0;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -26,70 +29,47 @@ public class BossMovement : MonoBehaviour
     {
         //Movement
 
+        //Determine Spawn Point
+        if (moving == false)
+        {
+            moving = true;
+            SpawnPoint();
+        }
 
-        switch (Reroll())
+        //Move
+        Movement();
+
+        //Change Position
+        switch (spawnPos)
         {
             case 0:
-                Debug.Log("Upper West to East");
-                newRotation = new Vector3(0, 270, 0);
-                transform.eulerAngles = newRotation;
-                transform.position = new Vector3(-57f, 0, 10);
-                transform.eulerAngles = newRotation;
+                if (transform.position.x >= maxX)
+                {
+                    SpawnPoint();
+                }
                 break;
 
             case 1:
-                Debug.Log("Lower West to East");
-                newRotation = new Vector3(0, 270, 0);
-                transform.eulerAngles = newRotation;
-                transform.position = new Vector3(-57f, 0, -17);
+                if (transform.position.z >= maxZ)
+                {
+                    SpawnPoint();
+                }
                 break;
 
             case 2:
-                Debug.Log("Left South to North");
-                newRotation = new Vector3(0, 180, 0);
-                transform.eulerAngles = newRotation;
-                transform.position = new Vector3(-29, 0, -56);
+                if (transform.position.x <= minX)
+                {
+                    SpawnPoint();
+                }
                 break;
 
             case 3:
-                Debug.Log("Right South to North");
-                newRotation = new Vector3(0, 180, 0);
-                transform.eulerAngles = newRotation;
-                transform.position = new Vector3(18, 0, -56);
-
+                if (transform.position.z <= minZ)
+                {
+                    SpawnPoint();
+                }
                 break;
 
-            case 4:
-                Debug.Log("Upper East to West");
-                newRotation = new Vector3(0, 90, 0);
-                transform.eulerAngles = newRotation;
-                transform.position = new Vector3(65, 0, -5);
-                break;
-
-            case 5:
-                Debug.Log("Lower East to West");
-                newRotation = new Vector3(0, 90, 0);
-                transform.eulerAngles = newRotation;
-                transform.position = new Vector3(65, 0, -28);
-                break;
-
-            case 6:
-                Debug.Log("Left North to South");
-                newRotation = new Vector3(0, 0, 0);
-                transform.eulerAngles = newRotation;
-                transform.position = new Vector3(-16, 0, 40);
-                break;
-
-            case 7:
-                Debug.Log("Right North to South");
-                newRotation = new Vector3(0, 0, 0);
-                transform.eulerAngles = newRotation;
-                transform.position = new Vector3(29, 0, 37);
-                break;
-
-            default:
-                Debug.Log("Nothing's working!");
-                break;
         }
 
         //Shoot Bullets
@@ -102,6 +82,118 @@ public class BossMovement : MonoBehaviour
             Despawn();
         }
     }
+
+    private void Movement()
+    {
+        switch (spawnPos)
+        {
+            case 0: //Move right
+                transform.position += Vector3.right * speed * Time.deltaTime;
+
+                break;
+
+            case 1: //Move Up
+                transform.position += Vector3.forward * speed * Time.deltaTime;
+                break; 
+            
+            
+            case 2: //Move Left
+                transform.position += Vector3.left * speed * Time.deltaTime;
+                break;
+
+
+            case 3: //Move Down
+                transform.position -= Vector3.forward * speed * Time.deltaTime;
+
+
+                break;
+
+
+            default:
+
+
+
+                break;
+        }
+    }
+
+    private void SpawnPoint()
+    {
+
+        switch (Reroll())
+        {
+            case 0:
+                Debug.Log("Upper West to East");
+                newRotation = new Vector3(0, 270, 0);
+                transform.eulerAngles = newRotation;
+                transform.position = new Vector3(-57f, 0, 10);
+                transform.eulerAngles = newRotation;
+                spawnPos = 0;
+                break;
+
+            case 1:
+                Debug.Log("Lower West to East");
+                newRotation = new Vector3(0, 270, 0);
+                transform.eulerAngles = newRotation;
+                transform.position = new Vector3(-57f, 0, -17);
+                spawnPos = 0;
+                break;
+
+            case 2:
+                Debug.Log("Left South to North");
+                newRotation = new Vector3(0, 180, 0);
+                transform.eulerAngles = newRotation;
+                transform.position = new Vector3(-29, 0, -56);
+                spawnPos = 1;
+                break;
+
+            case 3:
+                Debug.Log("Right South to North");
+                newRotation = new Vector3(0, 180, 0);
+                transform.eulerAngles = newRotation;
+                transform.position = new Vector3(18, 0, -56);
+                spawnPos = 1;
+                break;
+
+            case 4:
+                Debug.Log("Upper East to West");
+                newRotation = new Vector3(0, 90, 0);
+                transform.eulerAngles = newRotation;
+                transform.position = new Vector3(65, 0, -5);
+                spawnPos = 2;
+                break;
+
+            case 5:
+                Debug.Log("Lower East to West");
+                newRotation = new Vector3(0, 90, 0);
+                transform.eulerAngles = newRotation;
+                transform.position = new Vector3(65, 0, -28);
+                spawnPos = 2;
+                break;
+
+            case 6:
+                Debug.Log("Left North to South");
+                newRotation = new Vector3(0, 0, 0);
+                transform.eulerAngles = newRotation;
+                transform.position = new Vector3(-16, 0, 40);
+                spawnPos = 3;
+                break;
+
+            case 7:
+                Debug.Log("Right North to South");
+                newRotation = new Vector3(0, 0, 0);
+                transform.eulerAngles = newRotation;
+                transform.position = new Vector3(29, 0, 37);
+                spawnPos = 3;
+                break;
+
+            default:
+                Debug.Log("Nothing's working!");
+                break;
+
+        }
+    }
+
 
     private void Despawn()
     {
