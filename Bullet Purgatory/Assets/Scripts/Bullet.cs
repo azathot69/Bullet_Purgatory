9fd1a0fe4b68413af0be7bd50fe35fb972760f9d
maxohernandez <mohernandez@csuchico.edu>
2023-12-07 14:50:33 -0800

    Revert "Merge branch 'main' of https://github.com/azathot69/Bullet_Purgatory"
    This reverts commit 56c86eb062be03a1524142354266daa219bdd5f3, reversing
    changes made to c4204020cd059648dcbdfeabe00259046aa1b5b8.

--- Bullet Purgatory/Assets/Scripts/Bullet.cs
+++ Bullet Purgatory/Assets/Scripts/Bullet.cs
@@ -4,22 +4,23 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
-
     public float bulletLife = 1f;
+    public float rotation = 0f;
+    public float speed = 1f;
 
-
-
+    private Vector3 spawnPoint;
     private float timer = 0f;
 
-
+    // Start is called before the first frame update
+    void Start()
+    {
+        spawnPoint = new Vector3(transform.position.x, transform.position.y, transform.position.z);
+    }
 
     // Update is called once per frame
     void Update()
     {
         if (timer > bulletLife) Destroy(this.gameObject);
         timer += Time.deltaTime;
-
     }
-
-
 }
